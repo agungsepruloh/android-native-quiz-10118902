d192210018154f844c4d3ec5024ef02f95e10bda
Agung Sepruloh <agungsepruloh1996@gmail.com>
2021-05-29 12:52:38 +0700

    Implement user interface of the app

--- app/src/main/java/com/example/quiz10118902/MainActivity.kt
+++ app/src/main/java/com/example/quiz10118902/MainActivity.kt
@@ -3,6 +3,24 @@ package com.example.quiz10118902
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ *
+ * =========================================================================================
+ * This project was created to fulfill the quiz of Mobile Computing Application course.
+ *
+ *                              Universitas Komputer Indonesia
+ * =========================================================================================
+ *
+ * Author's Information
+ * ==========================
+ * NIM      : 10118902
+ * Name     : Agung Sepruloh
+ * Class    : IF-11
+ * Informatics Engineering
+ * ==========================
+ * Created on  29 May 2021
+ *
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
